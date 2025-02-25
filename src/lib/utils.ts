453057dd2d7268dd3ceae6e4bc476b6e0097f3d0
ghbishal <bishalk2056@gmail.com>
2025-02-26 04:25:09 +0900

    fix: date format parenthesis

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 import type { ClassValue } from 'clsx';
 import clsx from 'clsx';
-import { twMerge } from 'tailwind-merge';
 import { getLocales } from 'expo-localization';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -15,15 +15,19 @@ export function formatDate(
   const date = new Date(timestamp);
 
   if (formatType === 'fullDate') {
-    return date
+    const weekday = date
+      .toLocaleDateString(userLocale, { weekday: 'short' })
+      .replace(/,/g, '')
+      .toUpperCase();
+
+    const formattedDate = date
       .toLocaleDateString(userLocale, {
         year: 'numeric',
         month: '2-digit',
         day: '2-digit',
-        weekday: 'short',
       })
-      .replace(/,/g, '')
-      .toUpperCase();
+      .replace(/\//g, '/');
+    return `${formattedDate} (${weekday})`;
   }
 
   return date.toLocaleTimeString(userLocale, {
