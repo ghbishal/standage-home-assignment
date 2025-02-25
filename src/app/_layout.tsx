c36a191eae18ef3cbe49170b6b867114a7610450
ghbishal <bishalk2056@gmail.com>
2025-02-25 17:29:15 +0900

    feat: chat tab navigation

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,15 +3,48 @@ import '../../global.css';
 
 import { Stack } from 'expo-router';
 import { type ReactNode } from 'react';
+import { Image, TouchableOpacity } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StandageLogo } from '@/components/standageLogo';
 
 export default function RootLayout() {
   return (
     <RootProvider>
       <Stack>
         <Stack.Screen name="index" options={{ title: 'Home' }} />
-        <Stack.Screen name="chatList" options={{ title: 'Chat List' }} />
+        <Stack.Screen
+          name="(chatList)"
+          options={{
+            headerLeft: () => (
+              <TouchableOpacity
+                onPress={() => console.log('logout and other options')}
+              >
+                <Image
+                  className="size-10 rounded-full"
+                  source={{
+                    uri: 'https://images.unsplash.com/photo-1738255594069-76385a01a31d?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                  }}
+                />
+              </TouchableOpacity>
+            ),
+            headerTitle: () => <StandageLogo width={100} />,
+            headerRight: () => (
+              <TouchableOpacity
+                onPress={() =>
+                  console.log('open side drawer to language change')
+                }
+              >
+                <Image
+                  className="size-6 rounded-full"
+                  source={{
+                    uri: 'https://cdn-icons-png.flaticon.com/128/2040/2040504.png',
+                  }}
+                />
+              </TouchableOpacity>
+            ),
+          }}
+        />
       </Stack>
     </RootProvider>
   );
