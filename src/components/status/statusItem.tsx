a7a12c3ada668b18f2c6aca75524f83c4256de6a
ghbishal <bishalk2056@gmail.com>
2025-02-26 04:29:42 +0900

    feat: status item component

---
+++ src/components/status/statusItem.tsx
@@ -0,0 +1,52 @@
+import { useTranslation } from 'react-i18next';
+import { Text, View, Image } from 'react-native';
+import { ToolChip } from '../ui/toolChip';
+import { STATUS_STYLES } from './constants';
+import { cn } from '@/lib/utils';
+
+type Props = {
+  time?: string;
+  statuses?: string[];
+  isLastItem?: boolean;
+};
+export function StatusItem({ time, statuses, isLastItem = false }: Props) {
+  const { t } = useTranslation('translation', { keyPrefix: 'status' });
+  return (
+    <View
+      className={cn(
+        'flex-row items-center justify-between py-1',
+        isLastItem ? '' : 'border-b border-gray-300'
+      )}
+    >
+      <View className="mr-1 flex-row items-center gap-1">
+        <ToolChip label={time} variant="pressed" size="sm" />
+        <Image
+          source={{
+            uri: 'https://cdn-icons-png.flaticon.com/128/2989/2989838.png',
+          }}
+          className="size-5"
+        />
+        <Text className="text-sm text-gray-600">{t('status_changed')}</Text>
+      </View>
+      <View className="flex-row items-center gap-2">
+        {statuses &&
+          statuses.map((status, index) => (
+            <View
+              key={`${time}-${index}`}
+              className="flex-row items-center gap-1"
+            >
+              {index > 0 && <Text className="text-gray-500">→</Text>}
+              <Text
+                className={cn(
+                  'rounded-md px-2 py-1 text-xs font-semibold uppercase',
+                  STATUS_STYLES[status] || 'bg-gray-300'
+                )}
+              >
+                {t(status)}
+              </Text>
+            </View>
+          ))}
+      </View>
+    </View>
+  );
+}
