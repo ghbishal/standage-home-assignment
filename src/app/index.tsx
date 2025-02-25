0982a366c470da79227b99b94bb7ef69efdd6b34
ghbishal <bishalk2056@gmail.com>
2025-02-25 13:10:17 +0900

    feat: chat least screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,17 @@
+import { useRouter } from 'expo-router';
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Button } from '@/components/ui/button';
 
 export default function StartPage() {
+  const router = useRouter();
   return (
     <View className="flex-1 items-center justify-center">
       <Text>Hello World</Text>
+      <Button
+        label="Go To Chat Page"
+        onPress={() => router.push('/chatList')}
+      />
     </View>
   );
 }
