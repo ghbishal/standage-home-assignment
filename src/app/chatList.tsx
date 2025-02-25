a99f466a3e32b9b87f57fe57520f9d036c3c918f
ghbishal <bishalk2056@gmail.com>
2025-02-25 14:48:50 +0900

    feat: reply message

--- src/app/chatList.tsx
+++ src/app/chatList.tsx
@@ -9,17 +9,24 @@ import {
 
 import { ChatInput } from '@/components/chatInput';
 import { ChatItem } from '@/components/chatItem';
-import { messages, type MessageType } from '@/lib/chatData';
+import {
+  messages,
+  type ReplyMessageType,
+  type MessageType,
+} from '@/lib/chatData';
 
 export default function ChatList() {
   const [messageList, setMessageList] = useState<MessageType[]>(messages);
+  const [replyMessage, setReplyMessage] = useState<ReplyMessageType | null>(
+    null
+  );
   const messageRef = useRef<ScrollView>(null);
 
+  const clearReplyMessage = () => setReplyMessage(null);
+
   useEffect(() => {
-    if (messageRef.current) {
-      messageRef.current.scrollToEnd({ animated: true });
-    }
-  }, [messageList]);
+    scrollToEnd();
+  }, [messageList, replyMessage]);
 
   const handleSendMessage = (newMessage: string) => {
     if (!newMessage.trim()) return;
@@ -29,10 +36,20 @@ export default function ChatList() {
       sender: 'Germineau Nicolas',
       message: newMessage,
       timestamp: new Date().toISOString(),
+      replyTo: replyMessage,
     };
     setMessageList((prev) => [...prev, newMsg]);
+    setReplyMessage(null);
+    scrollToEnd();
   };
 
+  function scrollToEnd() {
+    // TODO: fix:- scroll to end not working
+
+    setTimeout(() => {
+      messageRef?.current?.scrollToEnd({ animated: true });
+    }, 100);
+  }
   return (
     <SafeAreaView className="flex-1">
       <KeyboardAvoidingView
@@ -46,13 +63,22 @@ export default function ChatList() {
           keyboardShouldPersistTaps="handled"
           showsVerticalScrollIndicator={false}
         >
-          <View className="mt-3 flex-1 gap-3 px-3">
+          <View className="my-2 flex-1 gap-2 px-3">
             {messageList.map((item) => (
-              <ChatItem message={item} key={item.id} />
+              <ChatItem
+                message={item}
+                key={item.id}
+                setReplyMessage={setReplyMessage}
+              />
             ))}
           </View>
         </ScrollView>
-        <ChatInput onSendMessage={handleSendMessage} />
+        <ChatInput
+          replyMessage={replyMessage}
+          clearReply={clearReplyMessage}
+          onSendMessage={handleSendMessage}
+          scrollToEnd={scrollToEnd}
+        />
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
