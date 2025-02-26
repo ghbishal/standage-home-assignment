1274f55a8f4ceb623b25c0cdce8f6d9491129dfa
ghbishal <bishalk2056@gmail.com>
2025-02-26 14:04:52 +0900

    chore: internal external translate

--- src/app/(chatList)/internalChat.tsx
+++ src/app/(chatList)/internalChat.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { View, Text } from 'react-native';
 
 export default function InternalChat() {
+  const { t } = useTranslation('translation', { keyPrefix: 'navigation' });
   return (
     <View>
-      <Text>InternalChat</Text>
+      <Text>{t('internal_chat')}</Text>
     </View>
   );
 }
